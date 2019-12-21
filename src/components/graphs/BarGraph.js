d01427e9f3de5d58edf20b02de640ffc9c85202f
Josh Mabry <mabry1985@gmail.com>
2019-12-21 10:52:16 -0800

    add axes to bar grpah

--- src/components/graphs/BarGraph.js
+++ src/components/graphs/BarGraph.js
@@ -3,6 +3,7 @@ import axios from "axios";
 import { Bar } from "@vx/shape";
 import { Group } from "@vx/group";
 import { scaleBand, scaleLinear } from "@vx/scale";
+import { AxisLeft, AxisBottom } from "@vx/axis";
 
 class BarGraph extends React.Component {
   constructor(props) {
@@ -26,35 +27,48 @@ class BarGraph extends React.Component {
     const yMax = this.state.height - this.state.margin.top - this.state.margin.bottom;
 
     const x = d => d.query;
-    const y = d => +d.count * 100;
+    const y = d => +d.count;
 
     const xScale = scaleBand({
       rangeRound: [0, xMax],
       domain: this.state.data.map(x),
-      padding: 0.4,
+      padding: 0.3,
     });
     const yScale = scaleLinear({
       rangeRound: [yMax, 0],
       domain: [0, Math.max(...this.state.data.map(y))],
     });
 
+
     const compose = (scale, accessor) => data => scale(accessor(data));
     const xPoint = compose(xScale, x);
     const yPoint = compose(yScale, y);
 
     return (
-      <svg width={this.state.width} height={this.state.height}>
+      <svg width={this.state.width} height={this.state.height}>          
+        <AxisBottom
+          top={yMax}
+          scale={xScale}
+          numTicks={this.state.width > 520 ? 10 : 5}
+        />
+        <AxisLeft
+          top={this.state.margin.top}
+          left={this.state.margin.left + 2}
+          scale={yScale}
+          numTicks={6}
+          tickFormat={this.yScaleFormat}
+        />
         {this.state.data.map((d, i) => {
           const barHeight = yMax - yPoint(d);
-          console.log(xScale.bandwidth())
           return (
             <Group key={`bar-${i}`}>
+
               <Bar
                 x={xPoint(d)}
                 y={yMax - barHeight}
                 height={barHeight}
                 width={xScale.bandwidth()}
-                fill="#fc2e1c"
+                fill="indigo"
               />
             </Group>
           );
