1ad583f91be326e449b2b4fa199908a3f44fd93a
Josh Mabry <mabry1985@gmail.com>
2019-12-22 11:29:37 -0800

    remove dummy data

--- src/App.js
+++ src/App.js
@@ -1,34 +1,17 @@
 import React from 'react';
 import GraphContainer from './components/graphs/GraphContainer';
 import './App.css';
+import ResultsContainer from './components/results/ResultsContainer';
 
 
 class App extends React.Component {
-  data = [
-    {
-      name: 'Backbone',
-      mentions: 2
-    },
-    {
-      name: 'Vue',
-      mentions: 5
-    },
-    {
-      name: 'React',
-      mentions: 14
-    },
-    {
-      name: 'Angular',
-      mentions: 10
-    }
-  ]
-
   
 
   render(){
     return (
       <div className="App">
-      <GraphContainer data={this.data}/>
+        <ResultsContainer/>
+        <GraphContainer/>
       </div>
     );
   }
