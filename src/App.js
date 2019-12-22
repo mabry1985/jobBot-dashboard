e6ae3ed804cf77e85d5829cc134072b013946169
Josh Mabry <mabry1985@gmail.com>
2019-12-22 14:54:59 -0800

    add simple flexbox

--- src/App.js
+++ src/App.js
@@ -2,16 +2,31 @@ import React from 'react';
 import GraphContainer from './components/graphs/GraphContainer';
 import './App.css';
 import ResultsContainer from './components/results/ResultsContainer';
+import { Column, Row } from "simple-flexbox";
 
 
 class App extends React.Component {
   
-
   render(){
     return (
-      <div className="App">
-        <ResultsContainer/>
-        <GraphContainer/>
+      <div className='container'>
+        <Column
+          alignItems='center'
+        >
+          <div className='container'>
+            <Row horizontal='space-between'>
+
+              <Column alignSelf='center'>
+                <ResultsContainer />
+              </Column>
+              
+              <Column alignSelf='center'>
+                <GraphContainer />
+              </Column>
+            
+            </Row>
+          </div>
+        </Column>
       </div>
     );
   }
