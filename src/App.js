b787851968ef5ecd1b77a3fd578ff016c2583449
Josh Mabry <mabry1985@gmail.com>
2019-12-23 09:16:51 -0800

    refactor to remove class

--- src/App.js
+++ src/App.js
@@ -4,30 +4,25 @@ import NavBar from './components/NavBar';
 import './App.css';
 import ResultsContainer from './components/results/ResultsContainer';
 import { Column, Row } from "simple-flexbox";
-import logo from './images/bot-logo.png';
 
-
-class App extends React.Component {
-
-  render(){
-    return (
-      <div>
-        <NavBar />
-        <Column flexGrow={1}>
-          <Row horizontal="center">
-          </Row>
-          <Row vertical="center" wrap="true">
-            <Column flexGrow={1} horizontal="center">
-              <ResultsContainer />
-            </Column>
-            <Column flexGrow={1} horizontal="center">
-              <GraphContainer />
-            </Column>
-          </Row>
-        </Column>
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div>
+      <NavBar />
+      <Column flexGrow={1}>
+        <Row horizontal="center">
+        </Row>
+        <Row vertical="center" wrap="true">
+          <Column flexGrow={1} horizontal="center">
+            <ResultsContainer />
+          </Column>
+          <Column flexGrow={1} horizontal="center">
+            <GraphContainer />
+          </Column>
+        </Row>
+      </Column>
+    </div>
+  );
 }
 
 export default App;
