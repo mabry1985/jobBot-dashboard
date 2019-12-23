fe0b26b811e3ba8ca1433137f0a9a7ea4da2b29f
Josh Mabry <mabry1985@gmail.com>
2019-12-23 13:50:28 -0800

    change background color and add dropshadow

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,18 +3,25 @@ import botLogo from '../images/bot-logo.png';
 
 const NavBar = props => {
   return (
-    <nav 
-      className="navbar navbar-light bg-light" 
-      style={{ borderBottom: 'solid 1px teal'}}>
-      <h4 className="navbar-brand">jobBot</h4>
-        <img 
-          src={botLogo} 
-          style={{ paddingTop: 18 }}
-          width="30" 
-          height="49" 
-          className="d-inline-block align-top" 
-          alt="bot-logo" 
-        />
+    <nav
+      className="navbar navbar-light bg-light"
+      style={{
+        backgroundColor: "#F8F8FF",
+        boxShadow:
+          "0 0px 8px 0 rgba(0, 0, 0, 0.2), 0 0px 15px 0 rgba(0, 0, 0, 0.19)"
+      }}
+    >
+      <h4 className="navbar-brand" style={{ paddingRight: 5 }}>
+        jobBot
+      </h4>
+      <img
+        src={botLogo}
+        style={{ paddingTop: 18 }}
+        width="30"
+        height="49"
+        className="d-inline-block align-top"
+        alt="bot-logo"
+      />
     </nav>
   );
 }
