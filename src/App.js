e5ea416744c6dd7fb4d1028950ea166cf066e2db
Josh Mabry <mabry1985@gmail.com>
2019-12-23 09:11:33 -0800

    remove hardcoded navbar

--- src/App.js
+++ src/App.js
@@ -1,27 +1,31 @@
 import React from 'react';
 import GraphContainer from './components/graphs/GraphContainer';
+import NavBar from './components/NavBar';
 import './App.css';
 import ResultsContainer from './components/results/ResultsContainer';
 import { Column, Row } from "simple-flexbox";
+import logo from './images/bot-logo.png';
 
 
 class App extends React.Component {
-  
+
   render(){
     return (
-      <Column flexGrow={1}>
-        <Row horizontal="center">
-          <h1 style={{ marginBottom: 20}}>jobBot</h1>
-        </Row>
-        <Row vertical="center">
-          <Column flexGrow={1} horizontal="center">
-            <ResultsContainer />
-          </Column>
-          <Column flexGrow={1} horizontal="center">
-            <GraphContainer />
-          </Column>
-        </Row>
-      </Column>
+      <div>
+        <NavBar />
+        <Column flexGrow={1}>
+          <Row horizontal="center">
+          </Row>
+          <Row vertical="center" wrap="true">
+            <Column flexGrow={1} horizontal="center">
+              <ResultsContainer />
+            </Column>
+            <Column flexGrow={1} horizontal="center">
+              <GraphContainer />
+            </Column>
+          </Row>
+        </Column>
+      </div>
     );
   }
 }
