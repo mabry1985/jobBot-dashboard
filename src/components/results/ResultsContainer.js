a143828e90c3fc87dbd44ab468e08f429dceef86
Josh Mabry <mabry1985@gmail.com>
2019-12-23 09:14:02 -0800

    add background color

--- src/components/results/ResultsContainer.js
+++ src/components/results/ResultsContainer.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 // import JobFilters from "./JobFilters"
 import JobResults from "./JobResults"
+
 import axios from "axios";
 
 class ResultsContainer extends Component {
@@ -9,8 +10,31 @@ class ResultsContainer extends Component {
 
     this.state = {
       endpoint: 'main',
+      loading: true,
       jobs: []
     };
+  }
+
+  truncate(string, length) {
+    let strArray = string.split(' ');
+    if (strArray.length < length){
+      return string;
+    } else {
+      strArray.splice(length, strArray.length);
+      const ellipsis = strArray.pop() + '...';
+      strArray.push(ellipsis);
+      string = strArray.join(' ');
+      return string;
+    }
+  }
+
+  manipulateJobData(jobs){
+    jobs.map(job => {
+      job.title = this.truncate(job.title, 4);    
+      job.timeStamp = new Date(job.timeStamp).toLocaleDateString();
+      return job
+    });
+    return jobs
   }
 
   componentDidMount() {
@@ -18,16 +42,22 @@ class ResultsContainer extends Component {
       .get("http://localhost:5000/jobs/")
       .then(response => {
         console.log(response);
-        this.setState({ jobs: response.data });
+        this.setState(
+          { 
+           jobs: this.manipulateJobData(response.data),
+           loading: false
+          });
       })
       .catch(error => {
         console.log(error);
       });
+
+    
   }
 
   render() {
     return (
-      <div className="container" style={{maxWidth:"500px"}}>
+      <div className="container">
         {/* <JobFilters/> */}
         <JobResults  jobs={this.state.jobs}/>
       </div>
