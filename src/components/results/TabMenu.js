0c20e91361f31d6169ea6bf64ff99f64c510a19f
Josh Mabry <mabry1985@gmail.com>
2019-12-24 15:38:26 -0800

    refactor layout

--- src/components/results/TabMenu.js
+++ src/components/results/TabMenu.js
@@ -2,17 +2,23 @@ import React from 'react';
 
 const TabMenu = (props) => {
   return (
-    <ul className="nav nav-pills" style={{ float: "right", marginRight: 55, marginTop: 30 }}>
+    <ul
+      className="nav nav-pills"
+      style={{ float: "right", marginRight: 55, marginTop: 30 }}
+    >
       <li className="nav-item">
         <button className="btn-primary" onClick={props.onClickJobs}>
           Jobs
         </button>
       </li>
       <li className="nav-item">
-        <button 
-          className="btn-primary" 
-          onClick={props.onClickQueries}>
+        <button className="btn-primary" onClick={props.onClickQueries}>
           Queries
+        </button>
+      </li>
+      <li className="nav-item">
+        <button className="btn-primary" onClick={props.onClickAdmin}>
+          Admin
         </button>
       </li>
     </ul>
