610447637e52c623dbadf41d09e465a5ca85681e
Josh Mabry <mabry1985@gmail.com>
2019-12-24 15:35:14 -0800

    seperate search components into own folder

---
+++ src/components/search/SearchQueries.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import axios from 'axios';
+const DataTable = require("react-data-components").DataTable;
+
+class SearchQueries extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      queriesList: [],
+      loading: true,
+      selected: ""
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("http://localhost:5000/search/")
+      .then(response => {
+        this.setState({
+          queriesList: response.data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  columns = [
+    { title: "Query", prop: "query" },
+  ];
+
+  selectRow = row => {
+    this.setState({ selected: row._id });
+  };
+
+  buildRowOptions = row => {
+    return {
+      onClick: this.selectRow.bind(this, row),
+      className: this.state.selected === row._id ? "active" : null
+    };
+  };
+
+  render() {
+    return (
+      <div
+        style={{
+          height: 600,
+          backgroundColor: "#f9f9f9",
+          padding: 40,
+          borderRadius: 5,
+          boxShadow:
+            "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)"
+        }}
+      >
+        <DataTable
+          buildRowOptions={this.buildRowOptions}
+          className="container queries"
+          id="query"
+          keys="_id"
+          columns={this.columns}
+          initialData={this.state.queriesList}
+          initialPageLength={10}
+        />
+      </div>
+    );
+  }
+}
+
+export default SearchQueries
