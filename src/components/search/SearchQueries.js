f823ec69d4e32c09505cb3ec5c216ff7c0229a8f
Josh Mabry <mabry1985@gmail.com>
2019-12-26 15:08:00 -0800

    add crud functionality to search

--- src/components/search/SearchQueries.js
+++ src/components/search/SearchQueries.js
@@ -47,8 +47,7 @@ class SearchQueries extends React.Component {
 
     this.state = {
       queriesList: [],
-      loading: true,
-      selected: ""
+      newQuery: ""
     };
   }
 
@@ -66,40 +65,131 @@ class SearchQueries extends React.Component {
       });
   }
 
-  columns = [
-    { title: "Query", prop: "query" },
-  ];
+  componentDidUpdate() {
+    axios
+      .get("http://localhost:5000/search/")
+      .then(response => {
+        this.setState({
+          queriesList: response.data,
+          loading: false
+        });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  }
+
+  columns = [{ title: "Query", prop: "query" }];
 
   selectRow = row => {
-    this.setState({ selected: row._id });
+    this.props.onClickQueryRow(row);
   };
 
   buildRowOptions = row => {
     return {
-      onClick: this.selectRow.bind(this, row),
-      className: this.state.selected === row._id ? "active" : null
+      onClick: this.selectRow.bind(this, row)
     };
   };
 
+  handleNewQuery = e => {
+    this.setState({
+      newQuery: e.target.value
+    });
+  }
+
+  handleSubmitNew = e => {
+    e.preventDefault();
+
+    const query = {
+      query: this.state.newQuery
+    };
+  
+    axios
+      .post("http://localhost:5000/search/add", query)
+      .then(res => console.log(res.data));
+      
+    this.setState(
+      { 
+        newQuery: "",
+      }
+    );
+  };
+
+  handleSubmitEdit = e => {
+    e.preventDefault();
+
+    const query = {
+      query: this.props.query
+    };
+
+    axios
+      .post("http://localhost:5000/search/update/" + this.props.queryId, query)
+      .then(res => console.log(res.data));
+    
+    this.props.onClearQueryPage();
+  };
+
+  handleDeleteQuery = () => {
+    axios
+      .delete("http://localhost:5000/search/" + this.props.queryId)
+      .then(response => {
+        console.log(response.data);
+      });
+  };
+
   render() {
+    const queryList = (
+      <div>
+        <InputDiv>
+          <form onSubmit={this.handleSubmitNew}>
+            <label htmlFor="add-query">Add Query:</label>
+            <br />
+            <input 
+              id="add-query" 
+              value={this.state.newQuery}
+              onChange={this.handleNewQuery} />
+            {/* <input 
+              type="submit" 
+              value="Submit" 
+              className="btn-primary" /> */}
+          </form>
+        </InputDiv>
+        <DataTable
+          buildRowOptions={this.buildRowOptions}
+          className="container queries"
+          id="query"
+          keys="_id"
+          columns={this.columns}
+          initialData={this.state.queriesList}
+          initialPageLength={9}
+        />
+      </div>
+    );
+
+    const editQuery = (
+      <div className="container">
+        <form onSubmit={this.handleSubmitEdit}>
+          <label htmlFor="">Edit Query:</label>
+          <br />
+          <input 
+            value={this.props.query} 
+            onChange={this.props.onChangeQuery} />
+          <input 
+            type="submit" 
+            value="Edit" 
+            className="btn-primary" />
+          <button className="btn-primary" onClick={this.handleDeleteQuery}>
+            Delete
+          </button>
+        </form>
+      </div>
+    );
+
+    const content = this.props.querySelected ? editQuery : queryList;
+
     return (
       <SearchDiv>
-        <StyledTableDiv>
-        <InputDiv>
-          <label htmlFor="">Add Query:</label>
-          <br/>
-          <input></input>
-        </InputDiv>
-          <DataTable
-            buildRowOptions={this.buildRowOptions}
-            className="container queries"
-            id="query"
-            keys="_id"
-            columns={this.columns}
-            initialData={this.state.queriesList}
-            initialPageLength={10}
-          />
-        </StyledTableDiv>
+        <StyledTableDiv>{content}</StyledTableDiv>
       </SearchDiv>
     );
   }
